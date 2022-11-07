7d88f457cc3d696fc3314fb6bcd0130e0c4d44c8
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2022-11-07 13:32:41 +0300

    Add functions to draw hits and misses

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,14 @@
 const grids = document.querySelectorAll('[data-ship-grid]');
 
+export const playerCells = document.querySelectorAll('[data-field="player"] [data-position]');
+export const enemyCells = document.querySelectorAll('[data-field="computer"] [data-position]');
+export const enemyGrid = document.querySelector('[data-field="computer"] [data-ship-grid]');
+
 // build grids
 const gridCell = function (row, column) {
   return `
     <div class="cell" data-position="${row}-${column}">
+      <div data-bullet="${row}-${column}"></div>
     </div>
   `;
 };
@@ -15,3 +20,24 @@ grids.forEach((grid) => {
     }
   }
 });
+
+export const drawShips = function (DOMCells, board, player) {
+  DOMCells.forEach((cell) => cell.classList.remove('ship'));
+  board.occupiedCells.forEach((cell) => {
+    document.querySelector(`[data-field="${player}"] [data-position="${cell}"]`).classList.add('ship');
+  });
+};
+
+export const drawMissed = function (board, player) {
+  if (board.missedShots.length === 0) return;
+  board.missedShots.forEach((coord) => {
+    document.querySelector(`[data-field=${player}] [data-bullet="${coord}"]`).classList.add('bullet');
+  });
+};
+
+export const drawHits = function (board, player) {
+  if (board.receivedHits.length === 0) return;
+  board.receivedHits.forEach((coord) => {
+    document.querySelector(`[data-field=${player}] [data-bullet="${coord}"]`).classList.add('bullet', 'hit');
+  });
+};
